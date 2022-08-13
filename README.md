207098a0a72c5e85170920defce2282490c4c273
Eric Chen <kych@berkeley.edu>
2022-08-12 22:19:18 -0700

    create a tuntap interface, need logic

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 
 
 # To run 
+
+Step 1: set up TUN TAP interface `tunros`
+```
+# terminal 1
+sudo python3 virtual_interface.py (the processing logic)
+
+# terminal 2 
+# setup the interface 
+sudo ip link set tunros up
+# dummy to trick ros to think it's an interface
+sudo ip addr add 192.0.2.1 peer 192.0.2.2 dev tunros 
+```
+
+Step 2: run ROS
+```
+# first export the path of ROS 
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp && export CYCLONEDDS_URI=file://`pwd`/cyclonedds.xml
+
+# publisher
+ros2 run demo_nodes_cpp talker
+# subscriber
+ros2 run demo_nodes_cpp listener
+```
+
+
---
+++ virtual_interface.py
@@ -0,0 +1,57 @@
+#from scapy.layers.tuntap import TunTapInterface
+import scapy.all as scapy
+from time import sleep 
+
+'''
+t = scapy.TunTapInterface('tun0')  
+while(True):
+    print(t.recv_raw())
+
+'''
+
+import fcntl
+import struct
+import os
+import socket
+import threading
+import sys
+
+TUNSETIFF = 0x400454ca
+TUNSETOWNER = TUNSETIFF + 2
+IFF_TUN = 0x0001
+IFF_TAP = 0x0002
+IFF_NO_PI = 0x1000
+
+
+def udp_send(dst, packet):
+    print ("udp_send")
+    sock.sendto(packet, (dst, 40000))
+
+def recv():
+     ss = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
+     ss.bind(("0.0.0.0", 40000))
+     while True:
+         data, addr = ss.recvfrom(1024)
+         print ("udp_recv")
+         os.write(tun.fileno(), data)
+
+if __name__ == "__main__":
+
+    dst = "127.1.1.1"
+    print ("Working on tunros inteface, destination address %s:40000 udp" % ( dst))
+    tun = open('/dev/net/tun', 'r+b', buffering = 0)
+    ifr = struct.pack('16sH', b'tunros', IFF_TUN | IFF_NO_PI)
+    fcntl.ioctl(tun, TUNSETIFF, ifr)
+    fcntl.ioctl(tun, TUNSETOWNER, 1000)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    t = threading.Thread(target=recv)
+    try:
+        t.start()
+        while True:
+            udp_send(dst, packet)
+            packet = os.read(tun.fileno(), 2048)
+            
+
+    except KeyboardInterrupt:
+        print("Terminating ...")
+        os._exit(0)
